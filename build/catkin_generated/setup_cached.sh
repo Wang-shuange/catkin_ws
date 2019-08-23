af684e11ba33e1e3fb46ac1684374cf7e3d0aeba
Wang-shuange <13213041229@163.com>
2019-08-23 17:29:15 +0800

    "first commit"

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/wangshuange/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/wangshuange/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/wangshuange/Android/flutter/bin:/usr/local/ffmpeg/bin/:/usr/local/ffmpeg/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/flutter/bin:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export PKG_CONFIG_PATH="/home/wangshuange/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/lib/pkgconfig"
+export PWD="/home/wangshuange/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/wangshuange/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/wangshuange/catkin_ws/src:$ROS_PACKAGE_PATH"
